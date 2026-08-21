8f835ed5aff6713cffebd447c3b3b4aeb4bcbca8
HagenK42 <666.thurisaz.666@gmail.com>
2026-08-21 13:51:04 +0200

    removed getFeeding functions from animal_repository

--- lib/core/database/repositories/animal_repository.dart
+++ lib/core/database/repositories/animal_repository.dart
@@ -1,8 +1,6 @@
 import 'package:drift/drift.dart';
 
 import '../app_database.dart';
-import '../tables/animals.dart';
-import '../tables/feeding_events.dart';
 
 class AnimalRepository {
   final AppDatabase database;
@@ -17,20 +15,6 @@ class AnimalRepository {
   
   Future<List<Animal>> getAllAnimals() {
     return database.select(database.animals).get();
-  }
-  
-  Future<DateTime?> getLastFeeding(int animalId) async {
-    final feeding = await (database.select(database.feedingEvents)
-      ..where((event) => event.animalId.equals(animalId))
-      ..orderBy([
-        (event) => OrderingTerm(
-          expression: event.fedAt,
-          mode: OrderingMode.desc,
-        ),
-      ])
-      ..limit(1))
-      .getSingleOrNull();
-    return feeding?.fedAt;
   }
 
   Future<List<Animal>> getAnimalsForBox(int boxId) {
